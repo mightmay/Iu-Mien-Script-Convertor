8577c1d9772af6f1bc5da30df6b7deb4a5e011a1
mightmay <mightlert@hotmail.com>
2020-04-15 01:54:54 -0400

    change search

--- MienScriptConvertor.py
+++ MienScriptConvertor.py
@@ -12,7 +12,6 @@ EXAMPLE
 """
 from lstm_seq_to_seq_restore import RNN_convertor
 #import pandas as pd
-from flask import Flask, jsonify, request
 #import pickle
 #import json
 from xml.etree import ElementTree
@@ -21,10 +20,11 @@ import re
 
 
 def search_node(from_script,to_script,input_text,root_node):
-    
+    print('XML search input_text'+input_text)
     for word in root_node:
 
         current_node_word=word.find(from_script)
+        
         if current_node_word is not None:
             if current_node_word.text == input_text:
                 current_equivalet_word=word.find(to_script)
@@ -62,8 +62,7 @@ def convert(from_script,to_script,input_text):
     RNNconvertor=RNN_convertor(data_path, model_path)
     splitted_input=re.split(reg_ex_match, word_input)
     for single_word in splitted_input:
-        print(single_word)
-        print(return_string)
+
         if single_word == '':
             continue
         
@@ -73,45 +72,48 @@ def convert(from_script,to_script,input_text):
             continue
             
         else:
-            transliterated_return_XML=search_node(from_script_XML_name,to_script_XML_name,input_text,ScriptMatchRoot)
-            if transliterated_return_XML is not None:
+            transliterated_return_XML=search_node(from_script_XML_name,to_script_XML_name,single_word,ScriptMatchRoot)
 
-                
+            if transliterated_return_XML is not None:
                 return_string=return_string+transliterated_return_XML
                 continue
             else:
                 
-                # try to search in the XML again with lowercase,remove ^ -
-                single_word_remove_carat=single_word.replace("^", "")
-                single_word_remove_dash=single_word_remove_carat.replace("-", "")
-                single_word=single_word_remove_dash
-                
                 if(from_script == 'MienOldRoman' or from_script=='MienNewRoman'):
-                    single_word_lower=single_word_remove_dash.lower()
+                    single_word_lower=single_word.lower()
                     single_word=single_word_lower
+
+                # try to search in the XML again with lowercase,remove ^ -
+                reg_ex_match_2='([.\[\{\(\)\}\]\,\;\?\!\^\-\s])'
+
+                splitted_input_2=re.split(reg_ex_match_2, single_word)
                 
-                #search again
-                transliterated_return_XML_2=search_node(from_script_XML_name,to_script_XML_name,input_text,ScriptMatchRoot)
-                if transliterated_return_XML_2 is not None:
-                    return_string=return_string+transliterated_return_XML_2
-                    continue
-
-                # try transcribing using SeqToSeq RNN
-                try:
-
-                    transliterated_return_RNN = RNNconvertor.transliterate_predict(single_word)
-                    transliterated_return_RNN=transliterated_return_RNN.rstrip('\n')
-
-                # if we also cannot use Sequence to sequence then just return the original string
-                except:
-                    transliterated_return_RNN = single_word
-                return_string=return_string+transliterated_return_RNN
+                for single_word_2 in splitted_input_2:
+                
+                    #search again
+                    transliterated_return_XML_2=search_node(from_script_XML_name,to_script_XML_name,single_word_2,ScriptMatchRoot)
+                    if transliterated_return_XML_2 is not None:
+                        return_string=return_string+transliterated_return_XML_2
+                        continue
+    
+                    # try transcribing using SeqToSeq RNN
+                    try:
+    
+                        transliterated_return_RNN = RNNconvertor.transliterate_predict(single_word_2)
+                        transliterated_return_RNN=transliterated_return_RNN.rstrip('\n')
+                        print('from Predict '+transliterated_return_RNN)
+    
+                    # if we also cannot use Sequence to sequence then just return the original string
+                    except:
+                        transliterated_return_RNN = single_word_2
+                    return_string=return_string+transliterated_return_RNN
 
     # return data
     return (return_string)
 
 # Test
-#word_input="หล,ง (ss); เยย?\nll!?\n"
+#word_input="หล,ง หลง (ss); เยย? ll!? นา"
+#word_input='Mienh nyei siang-Lomaa dora so nora oh nzaangc'
 #result=convert("MienThai","MienNewRoman",word_input)
-
+#print(result)
 
